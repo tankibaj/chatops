0057d30f5eabb3685195238c85a9a2935fefa3a5
Naim Ahmmed <mail@thenaim.com>
2023-06-30 11:00:15 +0200

    fix: argocd

--- modules/argocd.py
+++ modules/argocd.py
@@ -19,37 +19,142 @@ headers = {
 
 def get_argocd_applications():
     response = requests.get(f"{ARGOCD_URL}/api/v1/applications", headers=headers)
-    # return response.json()
     data = response.json()
     applications = data["items"]
-    results = []
+    return applications
 
-    for app in applications:
-        app_info = {
-            'name': app['metadata']['name'],
-            'sync_status': app['status']['sync']['status'],
-            'health_status': app['status']['health']['status'],
-            'sync_errors': app['status']['sync'].get('errorMessage', 'No sync errors'),
-            # -- To prevent hit OpenAI token limit only passing the above fields.
-            # 'project': app['spec']['project'],
-            # 'source_repo': app['spec']['source']['repoURL'],
-            # 'source_type': app['spec']['source'].get('chart', 'Git'),
-            'destination_server': app['spec']['destination'].get('server', 'No server provided'),
-            # 'destination_namespace': app['spec']['destination']['namespace']
-        }
-        results.append(app_info)
 
-    return json.dumps(results)
+def count_argocd_apps():
+    applications = get_argocd_applications()
+    return len(applications)
+
+
+def get_argocd_app_names():
+    applications = get_argocd_applications()
+    return [app['metadata']['name'] for app in applications]
+
+
+def get_status_apps(status):
+    applications = get_argocd_applications()
+    return [app for app in applications if app['status']['sync']['status'] == status]
+
+
+def count_status_apps(status):
+    return len(get_status_apps(status))
+
+
+def get_status_app_names(status):
+    apps = get_status_apps(status)
+    return [app['metadata']['name'] for app in apps]
+
+
+def get_sync_errors():
+    applications = get_argocd_applications()
+    return [app['status']['sync'].get('errorMessage', 'No sync errors') for app in applications if
+            app['status']['sync']['status'] == 'OutOfSync']
+
+
+def get_destination_server():
+    applications = get_argocd_applications()
+    return [app['spec']['destination'].get('server', 'No server provided') for app in applications]
 
 
 argocd_functions = {
     "get_argocd_applications": get_argocd_applications,
+    "count_argocd_apps": count_argocd_apps,
+    "get_argocd_app_names": get_argocd_app_names,
+    "get_status_apps": get_status_apps,
+    "count_status_apps": count_status_apps,
+    "get_status_app_names": get_status_app_names,
+    "get_sync_errors": get_sync_errors,
+    "get_destination_server": get_destination_server,
 }
 
 argocd_function_definitions = [
     {
         "name": "get_argocd_applications",
-        "description": "Get information about ArgoCD applications",
+        "description": "Fetches all ArgoCD applications from the ArgoCD server.",
+        "parameters": {
+            "type": "object",
+            "properties": {},
+            "required": []
+        }
+    },
+    {
+        "name": "count_argocd_apps",
+        "description": "Counts the total number of ArgoCD applications.",
+        "parameters": {
+            "type": "object",
+            "properties": {},
+            "required": []
+        }
+    },
+    {
+        "name": "get_argocd_app_names",
+        "description": "Retrieves a list of all ArgoCD application names.",
+        "parameters": {
+            "type": "object",
+            "properties": {},
+            "required": []
+        }
+    },
+    {
+        "name": "get_status_apps",
+        "description": "Retrieves applications with a specific sync status.",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "status": {
+                    "type": "string",
+                    "description": "The sync status to filter applications by. Should be one of 'Synced', "
+                                   "'OutOfSync', or 'Unknown'."
+                }
+            },
+            "required": ["status"]
+        }
+    },
+    {
+        "name": "count_status_apps",
+        "description": "Counts the number of applications with a specific sync status.",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "status": {
+                    "type": "string",
+                    "description": "The sync status to count applications by. Should be one of 'Synced', 'OutOfSync', "
+                                   "or 'Unknown'."
+                }
+            },
+            "required": ["status"]
+        }
+    },
+    {
+        "name": "get_status_app_names",
+        "description": "Retrieves a list of application names with a specific sync status.",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "status": {
+                    "type": "string",
+                    "description": "The sync status to filter application names by. Should be one of 'Synced', "
+                                   "'OutOfSync', or 'Unknown'."
+                }
+            },
+            "required": ["status"]
+        }
+    },
+    {
+        "name": "get_sync_errors",
+        "description": "Retrieves sync error messages for applications that are out of sync.",
+        "parameters": {
+            "type": "object",
+            "properties": {},
+            "required": []
+        }
+    },
+    {
+        "name": "get_destination_server",
+        "description": "Retrieves the destination server of each application.",
         "parameters": {
             "type": "object",
             "properties": {},
@@ -57,3 +162,4 @@ argocd_function_definitions = [
         }
     }
 ]
+
